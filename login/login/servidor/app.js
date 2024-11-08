6f4ea861ff0cc3cc96540b2b67a2197662725f51
chavow5 <davidramirezw5@gmail.com>
2024-11-08 15:38:39 -0300

    login

--- login/login/servidor/app.js
+++ login/login/servidor/app.js
@@ -3,14 +3,14 @@ import express from 'express';
 import authRoutes from './routes/auth.js';
 
 const app = express();
+// Middleware para manejar JSON
+app.use(express.json());
+
 // uso api/auth como ruta
 app.use('/api/auth', authRoutes);
 
 // puerto 
 const PORT = process.env.PORT || 5000;
-
-// Middleware para manejar JSON
-app.use(express.json());
 
 // Ruta
 app.get('/', (req, res) => {
