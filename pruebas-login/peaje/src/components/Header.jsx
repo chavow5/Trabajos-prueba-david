d241521d0264c7488435a39437d5fde373175311
chavow5 <davidramirezw6@gmail.com>
2024-11-12 14:30:03 -0300

    react

--- pruebas-login/peaje/src/components/Header.jsx
+++ pruebas-login/peaje/src/components/Header.jsx
@@ -6,7 +6,9 @@ const Header = () => (
     <h1>Sistema de Peaje</h1>
     <nav>
       <Link to="/">Inicio</Link>
-      <Link to="/login">Login</Link>
+      <Link to="/login">Iniciar Sesion </Link>
+      <Link to="/contacto">Contacto</Link>
+      
     </nav>
   </header>
 );
